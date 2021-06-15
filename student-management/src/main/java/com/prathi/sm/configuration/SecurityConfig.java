d7637d3e4920d79fc941ddac904551b23d5cf8e6
Pratheeban <pratheeban.eproc@gmail.com>
2021-06-15 11:04:32 +0530

    Spring security default schema added to load user credentials from DB

--- student-management/src/main/java/com/prathi/sm/configuration/SecurityConfig.java
+++ student-management/src/main/java/com/prathi/sm/configuration/SecurityConfig.java
@@ -1,17 +1,28 @@
 package com.prathi.sm.configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity(debug=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource datasource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		auth.inMemoryAuthentication().withUser("Prathi").password("{noop}Prathi123").roles("admin");
+//		auth.inMemoryAuthentication().withUser("Prathi").password("{noop}Prathi123").roles("admin");
+		
+		auth.jdbcAuthentication().dataSource(datasource).passwordEncoder(new BCryptPasswordEncoder());
+		
+		
 	}
 	
 	@Override
